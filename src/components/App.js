2776e092d5ff7a7a421e39dd420f2d1544b2aa65
Jaidev sah <jaidevsah007@gmail.com>
2023-01-16 12:44:50 +0530

    Initial Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
 
+  const [getIncre, setIncre] = useState(1);
+   
+  const IncreBtn = () => {
+      setIncre(getIncre+1);
+  }
+
+  const DecreBtn = () => {
+      setIncre(getIncre-1);
+  }
+
+  let className = "normal";
+
+  if(getIncre % 3 == 0){
+    className = "fizz";
+  }
+  else if(getIncre % 5 == 0){
+    className = "buzz";
+  }
+  else if(getIncre % 3 == 0 && getIncre % 5 == 0){
+    className = "fizzbuzz";
+  }
   
   return (
     <div id="main">
-
+       <button id='increment' onClick={IncreBtn}>Increment</button>
+       <div id='counter' className={className}>{getIncre}</div>
+       <button id='decrement' onClick={DecreBtn}>Decrement</button>
     </div>
   )
 }
